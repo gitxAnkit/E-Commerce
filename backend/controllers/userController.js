6fa77a314fb8631dd1374408723b0e0f6199af4d
Ankit <132276650+gitxAnkit@users.noreply.github.com>
2024-08-30 01:37:18 +0530

    User routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -155,3 +155,85 @@ exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
 
     sendToken(user, 200, res);
 });
+// Update user profile
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserDetail = {
+        name: req.body.name,
+        email: req.body.email
+    }
+    // Add cloudinary later
+
+
+    const user = await User.findByIdAndUpdate(req.user.id, newUserDetail, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+    res.status(200).json({
+        success: true,
+    });
+})
+// Get all users(admin)
+exports.getAllUser = catchAsyncErrors(async (req, res, next) => {
+    const users = await User.find();
+
+    res.status(200).json({
+        success: true,
+        users,
+    });
+});
+// Get single user (admin)
+exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return next(
+            new ErrorHandler(`User does not exist with Id: ${req.params.id}`)
+        );
+    }
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
+// update User Role -- Admin
+exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role,
+    };
+
+    await User.findByIdAndUpdate(req.params.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+
+    res.status(200).json({
+        success: true,
+    });
+});
+
+// Delete User --Admin
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return next(
+            new ErrorHandler(`User does not exist with Id: ${req.params.id}`, 400)
+        );
+    }
+
+    // const imageId = user.avatar.public_id;
+
+    // await cloudinary.v2.uploader.destroy(imageId);
+
+    // await user.remove();
+    await User.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+        success: true,
+        message: "User Deleted Successfully",
+    });
+});
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -11,13 +11,14 @@ const router = express.Router();
 router.route('/products')
     .get(handleGetAllProducts);
 
-router.route('/product/new')
+router.route('/admin/product/new')
     .post(isAuthenticatedUser, authorizedRoles("admin"), handleCreateProduct);
 
-router.route('/product/:id')
-    .get(isAuthenticatedUser, authorizedRoles("admin"), handleGetProductById)
+router.route('/admin/product/:id')
     .put(isAuthenticatedUser, authorizedRoles("admin"), handleUpdateProduct)
     .delete(isAuthenticatedUser, authorizedRoles("admin"), handleDeleteProduct);
 
+router.route('/product/:id')
+    .get(isAuthenticatedUser, authorizedRoles("admin"), handleGetProductById);
 
 module.exports = router; 
