71dae91cd1ac19736f57ceb6ab0e718dd86698a2
Ankit <132276650+gitxAnkit@users.noreply.github.com>
2024-10-10 00:15:52 +0530

    Integrate Stripe

--- backend/app.js
+++ backend/app.js
@@ -17,12 +17,13 @@ app.use(fileUpload());
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoutes");
+const payment = require("./routes/paymentRoute");
+
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
-
-
+app.use("/api/v1", payment);
 
 // Middleware for error
 app.use(errorMiddleware);
