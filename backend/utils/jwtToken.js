9722262d8592a5941952f85bffbc88de1ce9301b
Ankit <132276650+gitxAnkit@users.noreply.github.com>
2024-10-22 01:32:20 +0530

    commit

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,14 +1,14 @@
 const sendToken = (user, statusCode, res) => {
     const token = user.getJWTToken();
 
-    // options for cookie
+
     const options = {
         expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // Ensure COOKIE_EXPIRE is set
+            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true, // Ensure the cookie cannot be accessed via JavaScript
-        secure: process.env.NODE_ENV === 'production', // Send only over HTTPS in production
-        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Required for cross-site cookies in production
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
     };
 
     res.status(statusCode).cookie("token", token, options).json({
