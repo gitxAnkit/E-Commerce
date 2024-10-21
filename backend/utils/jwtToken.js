5fe0e975e2497f8dad8762cab9c9157df473713d
Ankit <132276650+gitxAnkit@users.noreply.github.com>
2024-10-22 01:42:03 +0530

    commit

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,7 +1,7 @@
 const sendToken = (user, statusCode, res) => {
     const token = user.getJWTToken();
 
-
+    // options for cookie
     const options = {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
@@ -17,3 +17,4 @@ const sendToken = (user, statusCode, res) => {
         token,
     });
 };
+module.exports = sendToken;
