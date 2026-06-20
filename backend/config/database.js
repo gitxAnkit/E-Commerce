3a5812d2f527c8b061acac04769eda8ec3df9ce2
Ankit <ankitv9936@gmail.com>
2026-06-21 00:09:59 +0530

    Update db config again

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Forces Node.js to use Google's DNS servers for SRV resolution
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = () => {
-
     mongoose
         .connect(process.env.DB_URI)
         .then((data) => {
             console.log(`MongoDB connected with server ${data.connection.host}`);
-        })
-}
+        });
+};
 
 module.exports = connectDB;
